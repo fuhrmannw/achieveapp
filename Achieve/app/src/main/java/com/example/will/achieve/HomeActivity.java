52ca61310a824759403086aed75f9bab41c3d0c5
William Fuhrmann <fuhrmann@iastate.edu>
2017-11-30 08:10:40 -0600

    worked on fragment replacement functions

--- Achieve/app/src/main/java/com/example/will/achieve/HomeActivity.java
+++ Achieve/app/src/main/java/com/example/will/achieve/HomeActivity.java
@@ -1,13 +1,55 @@
 package com.example.will.achieve;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fragment = new PostListFragment();
     }
+
+    public void toHomeFragment()
+    {
+        fragment = new PostListFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.post_list_fragment, fragment);
+        transaction.commit();
+    }
+
+    public void toFriendsFragment()
+    {
+        //fragment = new FriendsListFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        //transaction.replace(R.id.friends_list_fragment, fragment);
+        transaction.commit();
+    }
+
+    public void toProfileFragment()
+    {
+        //fragment = new ProfileListFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        //transaction.replace(R.id.profile_list_fragment, fragment);
+        transaction.commit();
+    }
+
+
+
+
+
 }
