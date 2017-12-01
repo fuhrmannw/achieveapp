323565fcdbf4bf140f5f502f3b75e23d4f3dd17b
William Fuhrmann <fuhrmann@iastate.edu>
2017-11-30 20:04:12 -0600

    added first name, last name, and username to profile page

--- Achieve/app/src/main/java/com/example/will/achieve/ProfileActivity.java
+++ Achieve/app/src/main/java/com/example/will/achieve/ProfileActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity implements PostListFragment.OnListFragmentInteractionListener {
 
@@ -14,6 +15,8 @@ public class ProfileActivity extends AppCompatActivity implements PostListFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        TextView tv = (TextView) findViewById(R.id.profileUsername);
+        tv.setText(Global.getInstance().getFirstName() + " " + Global.getInstance().getLastName() + " " + Global.getInstance().getUsername());
     }
 
     public void toFriendsActivity(View v)
